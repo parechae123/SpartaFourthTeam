924e316fd647f10d633efdfa2b065305985e44b0
Heoyoon <parechae@gmail.com>
2025-08-14 20:15:23 +0900

    Fix : 갑압판 구현 기반으로 객체 인터페이스 추가

--- Assets/Scripts/Box/BoxObject.cs
+++ Assets/Scripts/Box/BoxObject.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class BoxObject : MonoBehaviour , IGrabable,ICollideAction
 {
     [SerializeField] Rigidbody rb;
-
-    public Collider objectCollider { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
+    Collider col;
+    public Collider objectCollider { get => col; set => col= value; }
+    private void Start()
+    {
+        col = GetComponent<Collider>();
+    }
     public void OnCollide(Collider collider)
     {
         /*collider.GetComponents<PressurePlate>().SetValue*/
+        //
     }
 
     public void OnGrabEnter()
     {
+        rb.isKinematic = true;
         //transform.parent = 플레이어Transform;
         //transform.localPosition = 플레이어Transform.forward;
     }
     public void OnGrabExit()
     {
+        rb.isKinematic = false;
         transform.parent = null;
     }
     
