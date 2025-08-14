5b429a73220c2446911556cbee1c481e015445e7
Kseongmin1107 <ksmsky1107@gmail.com>
2025-08-14 14:33:39 +0900

    Feat_JumpPad

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JumpPad : MonoBehaviour , ICollideAction
+public class JumpPad : MonoBehaviour, ICollideAction
 {
     [SerializeField] private float jumpForce;
-
-    protected Collider collider { get; set; }
+    public Collider objectCollider { get; set; }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Rigidbody>() != null)
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+
+        if (rb != null)
         {
             OnCollide(other);
         }
     }
-
     public void OnCollide(Collider other)
     {
         Rigidbody rb = other.GetComponent<Rigidbody>();
 
+        if (rb == null) return;
+
+        Vector3 launchDirection = new Vector3(rb.velocity.x, 0, rb.velocity.z).normalized;
+
+        if (launchDirection == Vector3.zero)
+        {
+            launchDirection = Vector3.up;
+        }
+        else
+        {
+            launchDirection = (launchDirection + Vector3.up * 0.5f).normalized;
+        }
+
         rb.velocity = Vector3.zero;
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        rb.AddForce(launchDirection * jumpForce, ForceMode.Impulse);
     }
 }
